2cc792d372d1c966dfc2c71bd54186b2d83e22e4
Sumitkumbhar15 <sumitkumbhar204@gmail.com>
2025-09-19 19:13:04 +0530

    listing model

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const ejs = require("ejs");
-const port = 8080;
+const Listing = require("./models/listing.js");
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/stayease";
 
 main().then(() => {
     console.log("connected to database");
@@ -11,13 +13,26 @@ main().then(() => {
 })
 
 async function main(){
-    await mongoose.connect('mongodb://127.0.0.1:27017/stayease');
+    await mongoose.connect(MONGO_URL);
 }
 
 app.get("/" , (req,res) => {
     res.send("hii i am root");
 })
 
-app.listen(port , () => {
-    console.log(`Listening to port ${port}`);
+app.get("/testListing" ,async (req , res) => {
+    let sampleListing = new Listing ({
+        title : "Dazzle Villa",
+        description : "Villa that touches your heart by viewing nature",
+        price : 40999,
+        location : "karjat",
+        country : "India"
+    });
+    await sampleListing.save();
+    console.log("sample was saved");
+    res.send("successful");
+})
+
+app.listen(8080 , () => {
+    console.log(`Listening to port 8080`);
 })
