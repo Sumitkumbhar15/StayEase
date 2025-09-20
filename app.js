a68297d31696307cae90b3b5f69adad409eeac36
Sumitkumbhar15 <sumitkumbhar204@gmail.com>
2025-09-20 10:08:47 +0530

    index route

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const ejs = require("ejs");
 const Listing = require("./models/listing.js");
+const path = require("path");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/stayease";
 
@@ -16,22 +17,32 @@ async function main(){
     await mongoose.connect(MONGO_URL);
 }
 
+app.set("view engine" , "ejs");
+app.set(path.join(__dirname , "views"));
+
 app.get("/" , (req,res) => {
     res.send("hii i am root");
 })
 
-app.get("/testListing" ,async (req , res) => {
-    let sampleListing = new Listing ({
-        title : "Dazzle Villa",
-        description : "Villa that touches your heart by viewing nature",
-        price : 40999,
-        location : "karjat",
-        country : "India"
-    });
-    await sampleListing.save();
-    console.log("sample was saved");
-    res.send("successful");
+// Index route
+
+app.get("/listings" ,async (req , res) => {
+    const allListings =await Listing.find({});
+    res.render("./listings/index.ejs" , {allListings});
 })
+
+// app.get("/testListing" ,async (req , res) => {
+//     let sampleListing = new Listing ({
+//         title : "Dazzle Villa",
+//         description : "Villa that touches your heart by viewing nature",
+//         price : 40999,
+//         location : "karjat",
+//         country : "India"
+//     });
+//     await sampleListing.save();
+//     console.log("sample was saved");
+//     res.send("successful");
+// })
 
 app.listen(8080 , () => {
     console.log(`Listening to port 8080`);
