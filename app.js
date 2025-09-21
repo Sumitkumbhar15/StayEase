13d717873d02f35b3868e7b98076012c9dfe9a4e
Sumitkumbhar15 <sumitkumbhar204@gmail.com>
2025-09-21 10:44:42 +0530

    boilerplate , navbar , footer

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const ejs = require("ejs");
 const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/stayease";
 
@@ -22,6 +23,8 @@ app.set("view engine" , "ejs");
 app.set(path.join(__dirname , "views"));
 app.use(express.urlencoded({extended : true}));
 app.use(methodOverride("_method"));
+app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname , "/public")));
 
 app.get("/" , (req,res) => {
     res.send("hii i am root");
