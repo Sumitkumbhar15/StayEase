38eaf6ace3a2b715afa911b9621b0faebec4d686
Sumitkumbhar15 <sumitkumbhar204@gmail.com>
2025-09-28 17:22:53 +0530

    created review and validation

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-const {listingSchema} = require("./schema.js")
+const {listingSchema , reviewSchema} = require("./schema.js");
+const Review = require("./models/review.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/stayease";
 
@@ -35,6 +36,16 @@ app.get("/" , (req,res) => {
 
 const validateListing = (req , res , next) => {
     let error = listingSchema.validate(req.body);
+    if(error){
+        let errMsg = err.details.map((el) =>el.message).join(",");
+        throw(new ExpressError(400 , errMsg))
+    } else {
+        next();
+    }
+}
+
+const validateReview = (req , res , next) => {
+    let error = reviewSchema.validate(req.body);
     if(error){
         let errMsg = err.details.map((el) =>el.message).join(",");
         throw(new ExpressError(400 , errMsg))
@@ -94,6 +105,22 @@ app.delete("/listings/:id" ,wrapAsync(async (req , res) => {
     let {id} = req.params;
     await Listing.findByIdAndDelete(id);
     res.redirect("/listings");
+}))
+
+// Review
+
+// Post route
+
+app.post("/listings/:id/reviews" ,validateReview, wrapAsync(async (req , res) => {
+    let listing =await Listing.findById(req.params.id);
+    let newReview = new Review(req.body.review);
+
+    listing.reviews.push(newReview);
+
+    await newReview.save();
+    await listing.save();
+
+    res.redirect(`/listings/${listing._id}`)
 }))
 
 // app.get("/testListing" ,async (req , res) => {
