dfa195cfe73110cfa11c3376f4aa9f1f7d8d9e7e
Sumitkumbhar15 <sumitkumbhar204@gmail.com>
2025-09-29 16:57:25 +0530

    cookie parser package

--- classroom/server.js
+++ classroom/server.js
@@ -1,18 +1,39 @@
 const express = require("express");
 const app = express();
 const users = require("./routes/user.js");
-const posts = require("./routes/post.js")
+const posts = require("./routes/post.js");
+const cookieParser = require("cookie-parser");
 
-app.get("/" , (req , res ) => {
-    res.send("app is listening");
+app.use(cookieParser(secretcode));
+
+app.get("/getSignedCookie" , (req , res ) => {
+    res.cookie("Made-in" , "India" , {signed: true});
+    res.send("cookie sended");
 })
 
-app.use("/users" , users);
-app.use("/posts" , posts);
-
-
-
-
-app.listen(3000 , () => {
-    console.log("listening to port 3000");
+app.get("/verify" , (req , res ) => {
+    console.log(req.SignedCookies);
+    res.send("verified");
 })
+
+app.get("/getcookie" , (req , res) => {
+    res.cookie("greet" , "namaste");
+    res.send("our website store cookie");
+})
+
+app.get("/greet" , (req , res) => {
+    let {name} = req.cookies;
+    res.send(`hii ${name}`);
+})
+
+app.get("/", (req, res) => {
+    console.dir(req.cookies);
+  res.send("app is listening");
+});
+
+app.use("/users", users);
+app.use("/posts", posts);
+
+app.listen(3000, () => {
+  console.log("listening to port 3000");
+});
