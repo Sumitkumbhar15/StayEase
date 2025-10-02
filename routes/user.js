b36bc4111d2a5217df7434ae8244671fc00f423c
Sumitkumbhar15 <sumitkumbhar204@gmail.com>
2025-10-02 20:26:30 +0530

    login & signup using passport

--- routes/user.js
+++ routes/user.js
@@ -3,24 +3,42 @@ const router = express.Router();
 const User = require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync.js");
 const ExpressError = require("../utils/ExpressError.js");
+const passport = require("passport");
 
-router.get("/signup" , (req,res) => {
-    res.render("users/signup.ejs");
-})
+router.get("/signup", (req, res) => {
+  res.render("users/signup.ejs");
+});
 
-router.post("/signup" , wrapAsync(async (req,res) => {
-    try{
-    let { username , email , password } = req.body;
-    const newUser = new user({email , username});
-    const registeredUser = await User.register(newUser , password);
-    req.flash("success" , "User successfully registered");
-    res.redirect("/listings");
-    } catch(e) {
-        req.flash("error" , e.message);
-        res.redirect("/signup");
+router.post(
+  "/signup",
+  wrapAsync(async (req, res) => {
+    try {
+      let { username, email, password } = req.body;
+      const newUser = new User({ email, username });
+      const registeredUser = await User.register(newUser, password);
+      req.flash("success", "User successfully registered");
+      res.redirect("/listings");
+    } catch (e) {
+      req.flash("error", e.message);
+      res.redirect("/signup");
     }
-    
-}));
+  })
+);
+
+router.get("/login", (req, res) => {
+  res.render("users/login.ejs");
+});
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    failureRedirect: "/login",
+    failureFash: true,
+  }),
+  async (req, res) => {
+    res.flash("success" , "Welcome back to StayEase. ");
+    res.redirect("/listings");
+  }
+);
 
 module.exports = router;
-
