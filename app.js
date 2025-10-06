899712e65d4b5a41c93d2e83d44ac6a1f8ff8ff3
Sumitkumbhar15 <sumitkumbhar204@gmail.com>
2025-10-06 18:53:45 +0530

    mongo session store

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 if(process.env.NODE_ENV != "production") {
 require("dotenv").config();
 }
+
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
@@ -10,15 +11,16 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
+const MongoStore = require("connect-mongo")
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
 
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/stayease";
+// const MONGO_URL = "mongodb://127.0.0.1:27017/stayease";
 
-// const dbUrl = process.env.ATLASDB_URL;
+const dbUrl = process.env.ATLASDB_URL;
 
 const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
@@ -31,7 +33,7 @@ main().then(() => {
 })
 
 async function main(){
-    await mongoose.connect(MONGO_URL);
+    await mongoose.connect(dbUrl);
 }
 
 app.set("view engine" , "ejs");
@@ -41,7 +43,20 @@ app.use(methodOverride("_method"));
 app.engine('ejs', ejsMate);
 app.use(express.static(path.join(__dirname , "/public")));
 
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    crypto: {
+        secret: "mysupersecretstring"
+    },
+    touchAfter: 24 * 3600,
+});
+
+store.on("error" , () => {
+    console.log("Error in mongo session" , err);
+});
+
 const sessionOptions = {
+    store,
     secret: "mysupersecretstring",
     resave: false,
     saveUninitialized: true ,
